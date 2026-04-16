dd6ff24e96aca7f1ac18428f615adc1ae91f530c
Pankaj199923 <sainipankaj423@gmail.com>
2026-04-16 09:29:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -570,7 +570,7 @@ def render_header():
 # ======================
 render_header()
 
-tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10, tab11, tab12 = st.tabs([
+tab1, tab2, tab3, tab4, tab5, tab6, tab8, tab9, tab10, tab11, tab12 = st.tabs([
     "OPTION CHAIN",
     "SMART MONEY",
     "POSITIONS",
